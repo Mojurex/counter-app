5c51deed1f2a9c9b08df4d90728535e0b30b4bde
mojuh <mojuh05@gmail.com>
2025-07-21 17:47:16 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App(){
+  const[count, setCount] = useState(0);
+  const increase = () => setCount(count + 1);
+  const decrease = () => setCount(count - 1);
+  return (
+    <div className='App'>
+      <h1>Тоолуур</h1>
+      <h2>Тоо: {count}</h2>
+      <button onClick={increase}>+ Нэмэх</button>
+      <button onClick={decrease}>- Хасах</button>
+    </div>
+  );
+}
+
+export default App;
